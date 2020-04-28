a181ec27980b4f43200135fd093e7c08c84ac6fd
Brittany F <brittfranklin92@gmail.com>
2020-04-27 21:44:10 -0400

    String manipulation

---
+++ playground/types.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+/*
+const name = 'brittany';
+const middle = "monique";
+const last = `franklin`;
+
+const sentence = 'she\'s so cool';
+const sentence2 = `she's so "cool"`;
+
+const song = `Ohhhh
+
+yea
+
+I like
+
+pizza`;
+
+const hello = "hello my name is " + name +  ". Nice to meet you";
+const hello_backticks = `hello my name is ${name}. Nice to meet you.`;
+
+const html = `
+  <div>
+    <h2>${name}</h2>
+    <p>${hello}</p>
+  </div>
+`;
+
+document.body.innerHTML = html;
+*/
+
+const age = 100;
+const name = 'britt';
+
