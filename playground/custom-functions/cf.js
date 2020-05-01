bc6be40fa10bdbab14ad11e5ef5d9facc329ac53
brittmoni <brittfranklin92@gmail.com>
2020-04-30 20:36:26 -0400

    Playing with functions

--- playground/custom-functions/cf.js
+++ playground/custom-functions/cf.js
@@ -1,5 +1,5 @@
 function calculateBill(billAmount, taxRate) {
-  console.log('Running Calculate Bill!');
+  // console.log('Running Calculate Bill!');
   const total = billAmount * (1 + taxRate);
   return total;
 }
@@ -7,4 +7,15 @@ function calculateBill(billAmount, taxRate) {
 const myTotal = calculateBill(100, 0.13);
 const myTotal2 = calculateBill(200, 0.13);
 
-console.log(myTotal, myTotal2);
+// console.log(myTotal, myTotal2);
+
+function doctorize(name) {
+  return `Dr. ${name}`;
+}
+
+function yell(name) {
+  return `HEY ${name.toUpperCase()}`;
+}
+
+console.log(yell(doctorize('britt')));
+console.log(doctorize(yell('britt')));
